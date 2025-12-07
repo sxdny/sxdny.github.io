08e9fb1ba0f39d05ccf0b6d4caf6f92459e5e927
sxdny <67187745+sxdny@users.noreply.github.com>
2025-12-07 16:07:51 +0100

    feat: tweaks added
    more stable website (& files org)

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -6,12 +6,17 @@ export default function ShowProject({ project }: {
 }) {
   return (
     <div className="flex flex-col-reverse md:flex-row w-full pb-2 border-b">
-      <div className="py-2 w-1/8">
+      <div className="text-neutral-500 dark:text-neutral-400 py-2 w-1/8">
         <span> {project.year} </span>
       </div>
       <div className="flex flex-col gap-1 p-0 md:p-1 w-full">
         <a className="flex items-center hover:text-lime-600 dark:hover:text-lime-400 w-fit underline text-xl" href={project.url}> {project.name} <ArrowUpRight className="size-3.5" /> </a>
         <p className="text-md"> {project.description} </p>
+        <div className="text-neutral-500 dark:text-neutral-400 w-fit py-1 flex gap-3 items-bottom text-sm">
+          {project.technologies.map((tech) => (
+            <span> {tech} </span>
+          ))}
+        </div>
       </div>
     </div>
   )
